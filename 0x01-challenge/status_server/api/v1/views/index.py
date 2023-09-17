5b1e9b124718df15a8e76bc603e4bbf1f2673c88
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-09-17 11:46:12 +0100

    flask server

---
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Index view
+"""
+from flask import jsonify, Blueprint
+
+from api.v1.views import app_views
+
+api_views = Blueprint('api_views', __name__)
+
+
+@app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of the web server
+    """
+    return jsonify({"status": "OK"})
